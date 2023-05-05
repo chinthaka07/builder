0d731818e13d435875ba90c9ea5f0a4b320b317a
chinthaka <chinthakaweerasekara9@gmail.com>
2023-05-05 14:33:24 +0530

    3

--- src/ComputerShop.java
+++ src/ComputerShop.java
@@ -1,5 +1,12 @@
 public class ComputerShop {
     public static void main(String[] args) {
-
+        Computer samanC = new Computer(
+          "Lenovo",16,19,500L
+        );
+        Computer kamalC = new Computer(
+                "Mac",8,16,256L
+        );
+        System.out.println(samanC);
+        System.out.println(kamalC);
     }
 }
